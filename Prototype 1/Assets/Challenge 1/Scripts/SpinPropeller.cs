5830a02be659c716fe72abe92fd5e6208462a5cd
Laam19 <akib19@student.sust.edu>
2021-01-28 17:55:46 +0600

    Obstacle challenge

---
+++ Prototype 1/Assets/Challenge 1/Scripts/SpinPropeller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropeller : MonoBehaviour
+{
+    public GameObject Propeller;
+    private float propellerSpeed=20.0f;
+    private float propellerRotation=5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * propellerRotation * propellerSpeed);
+    }
+}
